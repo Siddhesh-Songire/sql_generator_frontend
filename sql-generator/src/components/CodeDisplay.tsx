dcc3629272c7d6e2d15759c689da64962dcbe2c6
Siddhesh <siddhehsongire@gmail.com>
2023-08-02 19:22:33 +0530

    Intialising project

---
+++ sql-generator/src/components/CodeDisplay.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CodeDisplay = () => {
+  return <div className="code-display">CodeDisplay</div>;
+};
+
+export default CodeDisplay;
---
+++ sql-generator/src/components/MessagesDisplay.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MessageDisplay from "./MessageDisplay";
+
+const MessagesDisplay = () => {
+  return (
+    <div className="message-display">
+      <MessageDisplay />
+    </div>
+  );
+};
+
+export default MessagesDisplay;
